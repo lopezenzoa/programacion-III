d5011fbd515b7462a60bcca1de6405d75619bccc
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2025-04-14 23:16:51 -0300

    changes returned value for method insertar()

--- tp-3/src/model/interfaces/I_Repositorio.java
+++ tp-3/src/model/interfaces/I_Repositorio.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface I_Repositorio<T> {
-    void insertar(T t) throws SQLException;
+    int insertar(T t) throws SQLException;
     List<T> listar() throws SQLException;
     T obtenerPorId(int id) throws SQLException;
     void actualizar(T nuevo) throws SQLException;
