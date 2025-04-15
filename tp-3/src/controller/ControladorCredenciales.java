8647be8ac09c99b15357e2e95766336d338e0d41
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2025-04-14 23:14:15 -0300

    adds basic features to controllers

--- tp-3/src/controller/ControladorCredenciales.java
+++ tp-3/src/controller/ControladorCredenciales.java
@@ -1,4 +1,29 @@
 package controller;
 
+import controller.enums.Permisos;
+import model.Credencial;
+import model.CredencialDAO;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 public class ControladorCredenciales {
+    private CredencialDAO modelo;
+
+    public ControladorCredenciales() throws SQLException {
+        this.modelo = new CredencialDAO();
+    }
+
+    public void agregarCredencial(int id_usuario, String username, String password, Permisos permisos) throws SQLException {
+        modelo.insertar(new Credencial(id_usuario, username, password, permisos));
+    }
+
+    public Credencial validarCredenciales(String username, String password) throws SQLException {
+        Optional<Credencial> credencial = modelo.obtenerPorUsername(username);
+
+        // retorno el ID del usuario para depues buscarlo en la tabla por el controlador correspondiente
+        return credencial.orElse(null);
+
+    }
 }
--- tp-3/src/controller/ControladorCuenta.java
+++ tp-3/src/controller/ControladorCuenta.java
@@ -1,4 +1,51 @@
 package controller;
 
+import controller.enums.TipoCuenta;
+import model.Cuenta;
+import model.CuentaDAO;
+import model.Usuario;
+import view.VistaCuenta;
+import view.VistaUsuario;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 public class ControladorCuenta {
+    private CuentaDAO modelo;
+
+    public ControladorCuenta() throws SQLException {
+        this.modelo = new CuentaDAO();
+    }
+
+    public void agregarCuenta(int id_usuario, TipoCuenta tipo, float saldo) throws SQLException {
+        modelo.insertar(new Cuenta(id_usuario, tipo, saldo));
+    }
+
+    public List<Cuenta> listarCuentas(int id_usuario) throws SQLException {
+        return modelo.listarPorUsuario(id_usuario);
+    }
+
+    public Optional<Cuenta> obtenerCuentaPorId(int id_cuenta) throws SQLException {
+        return Optional.of(modelo.obtenerPorId(id_cuenta));
+    }
+
+    public void mostrarCuenta(int id) throws SQLException {
+        Optional<Cuenta> cuenta = obtenerCuentaPorId(id);
+
+        cuenta.ifPresent(value -> VistaCuenta.mostrarCuenta(
+                value.getId_cuenta(),
+                value.getId_usuario(),
+                value.getTipo(),
+                value.getSaldo()
+        ));
+    }
+
+    public float saldoTotalPorCuenta(int id_usuario) throws SQLException {
+        List<Cuenta> cuentas = listarCuentas(id_usuario);
+
+        return cuentas.stream()
+                .map(Cuenta::getSaldo)
+                .reduce(0.0F, Float::sum);
+    }
 }
