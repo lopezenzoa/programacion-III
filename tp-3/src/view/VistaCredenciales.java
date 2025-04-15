6742c8f6dfa34f94ee8f62a2b1931945a1123fed
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2025-04-14 23:15:51 -0300

    adds basic methods to views

--- tp-3/src/view/VistaCredenciales.java
+++ tp-3/src/view/VistaCredenciales.java
@@ -1,4 +1,15 @@
 package view;
 
+import controller.enums.Permisos;
+
 public class VistaCredenciales {
+    public static void mostrarCredenciales(int id_credencial, int id_usuario, String username, String password, Permisos permisos) {
+        System.out.println("----- Credencial Información -----");
+        System.out.println("ID Credencial : " + id_credencial);
+        System.out.println("ID Usuario    : " + id_usuario);
+        System.out.println("Username      : " + username);
+        System.out.println("Password      : " + password);
+        System.out.println("Permisos      : " + permisos);
+        System.out.println("----------------------------------");
+    }
 }
--- tp-3/src/view/VistaCuenta.java
+++ tp-3/src/view/VistaCuenta.java
@@ -1,4 +1,14 @@
 package view;
 
+import controller.enums.TipoCuenta;
+
 public class VistaCuenta {
+    public static void mostrarCuenta(int id_cuenta, int id_usuario, TipoCuenta tipoCuenta, float saldo) {
+        System.out.println("----- Cuenta Información -----");
+        System.out.println("ID Cuenta   : " + id_cuenta);
+        System.out.println("ID Usuario  : " + id_usuario);
+        System.out.println("Tipo Cuenta : " + tipoCuenta);
+        System.out.println("Saldo       : $" + String.format("%.2f", saldo));
+        System.out.println("------------------------------");
+    }
 }
